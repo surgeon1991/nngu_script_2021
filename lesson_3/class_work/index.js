33c8f762d0f18b44772e994ed24f502b096afe33
Alexandr Ponomarev <surgeonist123456@yandex.ru>
2021-10-02 12:28:42 +0300

    учебный коммит

---
+++ lesson_3/class_work/index.js
@@ -0,0 +1,101 @@
+//String
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+
+// const obj = {
+//   foo: 67,
+// };
+
+// console.log(String(obj.foo));
+
+//number
+
+// let testValue;
+
+// console.log(testValue);
+
+// testValue = Number(testValue);
+
+// console.log(testValue);
+
+// const isUser = true;
+// const isAdmin = false;
+
+// console.log(Number(isAdmin), Number(isUser));
+
+// const voidString = "";
+
+// console.log(Number(voidString));
+
+Boolean;
+
+const value = "";
+
+console.log(typeof Boolean(value), Boolean(value));
+
+const obj = [].length;
+
+console.log(Boolean(obj));
+
+//математические операторы
+
+// console.log(16 % 3);
+// console.log(2 ** 2);
+
+//унарный плюс
+
+// const value = 5;
+
+// console.log(typeof +value, typeof +5);
+
+// const value = "5";
+
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+//бинарный плюс
+
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "12";
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(numberValue + +stringValue);
+
+// const firstName = "Alex";
+// const lastName = "Shirokov";
+
+// console.log(firstName + " " + lastName);
+
+// console.log(`${firstName} ${lastName}`);
+
+//операции сравнения
+
+// console.log(false != 0);
+
+// const firstString = "яблоко";
+// const secondString = "Яблоко";
+
+// console.log(firstString > secondString);
+
+// const a = 0;
+
+// const b = "0";
+
+// console.log(a == b);
+
+// console.log(a === b);
+
+// console.log("" - 1 + 0, typeof ("" - 1 + 0));
+// console.log(typeof (7 / 0));
+
+// console.log(null == "\n0\n");
+// console.log(null === +"\n0\n");
